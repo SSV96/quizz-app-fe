6f73be2e07a1d0da2dd42b0df693ad2116b83b36
sathya Swaroop <sathyaswaroopvandavasi@gmail.com>
2025-09-29 08:12:09 +0000

    removed uncessary state

--- src/components/canvas/Canvas.tsx
+++ src/components/canvas/Canvas.tsx
@@ -1,22 +1,10 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Droppable } from '@hello-pangea/dnd';
-import { useQuizStore } from '../../store/useQuizStore';
 import DraggableItem from './DraggableItem';
-import { DroppableAreaEnum, BlockEnum, Block, QuestionKindEnum, Quiz } from '../../types';
-import { demoQuiz } from '@/src/utils/demo-quiz';
+import { DroppableAreaEnum, Quiz } from '../../types';
 
 export default function Canvas({ quiz }: { quiz: Quiz }) {
-  const [blocks, setDisplayBlocks] = useState<Block[]>(quiz.blocks);
-
-  useEffect(() => {
-    if (!quiz.blocks || quiz.blocks.length === 0) {
-      setDisplayBlocks(demoQuiz[0].blocks);
-    } else {
-      setDisplayBlocks(quiz.blocks);
-    }
-  }, [quiz.blocks]);
-
   return (
     <div className="flex-1 p-6 bg-gray-50 transition-colors">
       <h3 className="font-bold mb-4 text-gray-700">Canvas</h3>
@@ -34,13 +22,8 @@ export default function Canvas({ quiz }: { quiz: Quiz }) {
               transition-colors
             "
           >
-            {blocks.map((block, i) => (
-              <DraggableItem
-                block={block}
-                index={i}
-                quizId={quiz?.id ?? 'default'}
-                key={block.id}
-              />
+            {quiz.blocks.map((block, i) => (
+              <DraggableItem block={block} index={i} quizId={quiz.id} key={block.id} />
             ))}
 
             {provided.placeholder}
